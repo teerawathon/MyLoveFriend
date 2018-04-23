a76316feda9bb2f566c567c3c449b5d5dbe34289
teerawat <teerawat@skyict.co.th>
2018-04-23 14:27:15 +0700

    Toolbar Success

--- app/src/main/java/teerawat/skyict/co/th/mylovefriend/fragment/RegiterFragment.java
+++ app/src/main/java/teerawat/skyict/co/th/mylovefriend/fragment/RegiterFragment.java
@@ -4,13 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import teerawat.skyict.co.th.mylovefriend.MainActivity;
 import teerawat.skyict.co.th.mylovefriend.R;
 
 public class RegiterFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Regiter");
+        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+
+//        Show Navigation Icon
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    } //OnActivity
 
     @Nullable
     @Override
