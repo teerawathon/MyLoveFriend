743c9e6954569e6b11706773709db77da3943f4c
teerawat <teerawat@skyict.co.th>
2018-04-23 15:15:39 +0700

    Choose Image Success

--- app/src/main/java/teerawat/skyict/co/th/mylovefriend/fragment/RegiterFragment.java
+++ app/src/main/java/teerawat/skyict/co/th/mylovefriend/fragment/RegiterFragment.java
@@ -1,5 +1,9 @@
 package teerawat.skyict.co.th.mylovefriend.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +12,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import teerawat.skyict.co.th.mylovefriend.MainActivity;
 import teerawat.skyict.co.th.mylovefriend.R;
 
 public class RegiterFragment extends Fragment{
+
+    //    set value
+    private Uri uri;
+    private ImageView imageView;
 
 
     @Override
@@ -20,12 +29,59 @@ public class RegiterFragment extends Fragment{
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
+
+//        Avatar Controller
+        avatarController();
+
+
+    } //OnActivity
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity()
+                        .getContentResolver()
+                        .openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } // if
+
+    }
+
+    private void avatarController() {
+
+        imageView = getView().findViewById(R.id.imvAvatar);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent to another App
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Please Choose Image"),
+                        1);
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
 
 //        Setup Title
-        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Regiter");
-        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+//        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
 
 //        Show Navigation Icon
         ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
@@ -37,8 +93,7 @@ public class RegiterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-    } //OnActivity
+    }
 
     @Nullable
     @Override
